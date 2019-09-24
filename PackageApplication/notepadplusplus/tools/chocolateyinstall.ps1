9f09fe283d7614efd325a3b027bd9057315382f6
Miso <miso.stamenic@gmail.com>
2019-09-24 15:21:10 +0200

    Added files for packaging applications

---
+++ PackageApplication/notepadplusplus/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'notepadplusplus'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'npp.7.7.Installer.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = $fileLocation
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-install-package
